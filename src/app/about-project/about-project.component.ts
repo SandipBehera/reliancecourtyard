046dc42bf4557d57b8280a28ee5b3f5be3f94c9d
SandipBehera <sandip.sudip36@gmail.com>
2025-09-10 23:36:42 +0530

    auto play

--- src/app/about-project/about-project.component.ts
+++ src/app/about-project/about-project.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, ElementRef, HostListener, Inject, PLATFORM_ID, ViewChild } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalComponent } from '../modal/modal.component';
 import { isPlatformBrowser } from '@angular/common';
@@ -11,6 +11,7 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './about-project.component.css'
 })
 export class AboutProjectComponent {
+  @ViewChild('bgVideo', { static: true }) bgVideo!: ElementRef<HTMLVideoElement>;
   ismobile = false;
   constructor(@Inject(PLATFORM_ID) private platformId: Object, private modalService: NgbModal,) {
   }
@@ -23,6 +24,20 @@ export class AboutProjectComponent {
     console.log('open', title);
     const modalRef = this.modalService.open(ModalComponent);
     modalRef.componentInstance.title = title;
+  }
+  ngAfterViewInit() {
+    const v = this.bgVideo.nativeElement;
+    v.muted = true;            // ensure muted at runtime
+    v.playsInline = true as any;
+    (v as any).webkitPlaysInline = true;
+    v.play().catch(() => { /* ignore; will retry on touch */ });
+  }
+
+  // As a fallback, start playback on the first tap
+  @HostListener('window:touchstart', ['$event'])
+  onFirstTouch() {
+    const v = this.bgVideo?.nativeElement;
+    if (v && v.paused) v.play().catch(() => {});
   }
 
 }
